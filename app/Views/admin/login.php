87ce995dd4b53bb193da2380a89c588b5a1982e5
fikri <fikri.aslam102519@students.unila.ac.id>
2023-12-12 05:55:19 +0700

    update icon header on login view

--- app/Views/admin/login.php
+++ app/Views/admin/login.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="<?=base_url('images/unila.png')?>"/>
     <title>Login Admin</title>
 
     <script src="https://cdn.tailwindcss.com"></script>
