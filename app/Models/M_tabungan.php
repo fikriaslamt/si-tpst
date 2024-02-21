2a805ea0b1313fa07e2d591d8ed03a0957309cc6
fikri <fikri.aslam102519@students.unila.ac.id>
2024-02-21 08:57:04 +0700

    update error

--- app/Models/M_tabungan.php
+++ app/Models/M_tabungan.php
@@ -45,15 +45,11 @@ class M_tabungan extends Model
         ->join('nasabah','tabungan.nasabah_id=nasabah.id')->findAll();
     }
 
-    function getSaldo($id){
+    function getSaldo($id) {
         $builder = $this->table('tabungan');
         $temp = $builder->where('nasabah_id', $id)->findAll();
-        $saldo = 0;
-        foreach($temp as $t){
-            $saldo = $t['saldo'];
-        }
-
-        return  $saldo;
+   
+        return $temp["saldo"];
     }
 
     function getTotalSaldo(){
